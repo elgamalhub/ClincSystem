7430cc342bccaa395d7d85315da8c90b6975f7b1
Ahmed Abdelrahman Elgamal <48561510+elgamalhub@users.noreply.github.com>
2025-05-03 00:30:44 +0300

    Add files via upload

---
+++ ClincSystemDatabase/init.sql
@@ -0,0 +1,162 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [AspNetRoles] (
+    [Id] nvarchar(450) NOT NULL,
+    [Name] nvarchar(256) NULL,
+    [NormalizedName] nvarchar(256) NULL,
+    [ConcurrencyStamp] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetRoles] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [AspNetUsers] (
+    [Id] nvarchar(450) NOT NULL,
+    [FullName] nvarchar(max) NOT NULL,
+    [UserName] nvarchar(256) NULL,
+    [NormalizedUserName] nvarchar(256) NULL,
+    [Email] nvarchar(256) NULL,
+    [NormalizedEmail] nvarchar(256) NULL,
+    [EmailConfirmed] bit NOT NULL,
+    [PasswordHash] nvarchar(max) NULL,
+    [SecurityStamp] nvarchar(max) NULL,
+    [ConcurrencyStamp] nvarchar(max) NULL,
+    [PhoneNumber] nvarchar(max) NULL,
+    [PhoneNumberConfirmed] bit NOT NULL,
+    [TwoFactorEnabled] bit NOT NULL,
+    [LockoutEnd] datetimeoffset NULL,
+    [LockoutEnabled] bit NOT NULL,
+    [AccessFailedCount] int NOT NULL,
+    CONSTRAINT [PK_AspNetUsers] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [Specializations] (
+    [SpecializationId] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Specializations] PRIMARY KEY ([SpecializationId])
+);
+
+CREATE TABLE [AspNetRoleClaims] (
+    [Id] int NOT NULL IDENTITY,
+    [RoleId] nvarchar(450) NOT NULL,
+    [ClaimType] nvarchar(max) NULL,
+    [ClaimValue] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [AspNetUserClaims] (
+    [Id] int NOT NULL IDENTITY,
+    [UserId] nvarchar(450) NOT NULL,
+    [ClaimType] nvarchar(max) NULL,
+    [ClaimValue] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [AspNetUserLogins] (
+    [LoginProvider] nvarchar(450) NOT NULL,
+    [ProviderKey] nvarchar(450) NOT NULL,
+    [ProviderDisplayName] nvarchar(max) NULL,
+    [UserId] nvarchar(450) NOT NULL,
+    CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY ([LoginProvider], [ProviderKey]),
+    CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [AspNetUserRoles] (
+    [UserId] nvarchar(450) NOT NULL,
+    [RoleId] nvarchar(450) NOT NULL,
+    CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY ([UserId], [RoleId]),
+    CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [AspNetUserTokens] (
+    [UserId] nvarchar(450) NOT NULL,
+    [LoginProvider] nvarchar(450) NOT NULL,
+    [Name] nvarchar(450) NOT NULL,
+    [Value] nvarchar(max) NULL,
+    CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY ([UserId], [LoginProvider], [Name]),
+    CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [Patients] (
+    [PatientId] nvarchar(450) NOT NULL,
+    [Gender] nvarchar(max) NOT NULL,
+    [DateOfBirth] datetime2 NULL,
+    [Phone] nvarchar(max) NOT NULL,
+    [UserId] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Patients] PRIMARY KEY ([PatientId]),
+    CONSTRAINT [FK_Patients_AspNetUsers_PatientId] FOREIGN KEY ([PatientId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [Doctors] (
+    [DoctorId] nvarchar(450) NOT NULL,
+    [Bio] nvarchar(max) NOT NULL,
+    [Phone] nvarchar(max) NOT NULL,
+    [SpecializationId] int NOT NULL,
+    [UserId] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Doctors] PRIMARY KEY ([DoctorId]),
+    CONSTRAINT [FK_Doctors_AspNetUsers_DoctorId] FOREIGN KEY ([DoctorId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Doctors_Specializations_SpecializationId] FOREIGN KEY ([SpecializationId]) REFERENCES [Specializations] ([SpecializationId]) ON DELETE CASCADE
+);
+
+CREATE TABLE [AppointmentSlots] (
+    [SlotId] int NOT NULL IDENTITY,
+    [Date] datetime2 NOT NULL,
+    [TimeFrom] time NOT NULL,
+    [TimeTo] time NOT NULL,
+    [IsBooked] bit NOT NULL,
+    [DoctorId] int NOT NULL,
+    [DoctorId1] nvarchar(450) NULL,
+    CONSTRAINT [PK_AppointmentSlots] PRIMARY KEY ([SlotId]),
+    CONSTRAINT [FK_AppointmentSlots_Doctors_DoctorId1] FOREIGN KEY ([DoctorId1]) REFERENCES [Doctors] ([DoctorId])
+);
+
+CREATE TABLE [Appointments] (
+    [AppointmentId] int NOT NULL IDENTITY,
+    [PatientId] int NOT NULL,
+    [PatientId1] nvarchar(450) NULL,
+    [SlotId] int NOT NULL,
+    [Status] nvarchar(max) NOT NULL,
+    [Notes] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Appointments] PRIMARY KEY ([AppointmentId]),
+    CONSTRAINT [FK_Appointments_AppointmentSlots_SlotId] FOREIGN KEY ([SlotId]) REFERENCES [AppointmentSlots] ([SlotId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Appointments_Patients_PatientId1] FOREIGN KEY ([PatientId1]) REFERENCES [Patients] ([PatientId])
+);
+
+CREATE INDEX [IX_Appointments_PatientId1] ON [Appointments] ([PatientId1]);
+
+CREATE UNIQUE INDEX [IX_Appointments_SlotId] ON [Appointments] ([SlotId]);
+
+CREATE INDEX [IX_AppointmentSlots_DoctorId1] ON [AppointmentSlots] ([DoctorId1]);
+
+CREATE INDEX [IX_AspNetRoleClaims_RoleId] ON [AspNetRoleClaims] ([RoleId]);
+
+CREATE UNIQUE INDEX [RoleNameIndex] ON [AspNetRoles] ([NormalizedName]) WHERE [NormalizedName] IS NOT NULL;
+
+CREATE INDEX [IX_AspNetUserClaims_UserId] ON [AspNetUserClaims] ([UserId]);
+
+CREATE INDEX [IX_AspNetUserLogins_UserId] ON [AspNetUserLogins] ([UserId]);
+
+CREATE INDEX [IX_AspNetUserRoles_RoleId] ON [AspNetUserRoles] ([RoleId]);
+
+CREATE INDEX [EmailIndex] ON [AspNetUsers] ([NormalizedEmail]);
+
+CREATE UNIQUE INDEX [UserNameIndex] ON [AspNetUsers] ([NormalizedUserName]) WHERE [NormalizedUserName] IS NOT NULL;
+
+CREATE INDEX [IX_Doctors_SpecializationId] ON [Doctors] ([SpecializationId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250502210818_Init', N'9.0.4');
+
+COMMIT;
+GO
+
